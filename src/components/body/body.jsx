bfe9993bccda0ebb92d2f746c8d6e3a42bf04c22
Gabriel Aristeu Cabral <gabriel@pythonic.ai>
2022-11-26 19:57:42 -0300

    Teoricamente está funcionando na prática não

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,19 +1,46 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 import "./body.css"
 import { useDropzone } from 'react-dropzone'
-import PdfViewerComponent from '../container/container';
 import assinar from '../misc/misc';
 
 function Body() {
   const [_file, setFile] = useState(null);
-  const [f_file, setFFile] = useState(null);
+  const [pdf_instance, setPDFInstance] = useState(null);
+  const [filename, setFilename] = useState(null);
   const onDrop = useCallback(acceptedFiles => {
     // Do something with the files
-    console.log(acceptedFiles[0]);
+    setFilename(acceptedFiles[0].name)
     acceptedFiles[0].arrayBuffer().then(value => setFile(value))
-    acceptedFiles[0].arrayBuffer().then(value => setFFile(value))
+    
   }, [])
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  const containerRef = useRef(null);
+  useEffect(() => {
+    console.log(_file)
+    if (_file) {
+      
+      const container = containerRef.current;
+      let PSPDFKit;
+
+      (async function () {
+        PSPDFKit = await import("pspdfkit");
+        setPDFInstance(await PSPDFKit.load({
+          // Container where PSPDFKit should be mounted.
+          container,
+          // The document to open.
+          document: _file,
+          // Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
+          baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`
+        })
+        )
+
+        pdf_instance.setToolbarItems([]);
+      })();
+      return () => PSPDFKit && PSPDFKit.unload(container);
+    }
+
+  }, [_file])
 
   return (
     <div className='Body'>
@@ -21,10 +48,10 @@ function Body() {
         {
           _file ?
             <div id="content">
-              <PdfViewerComponent document={_file} />
+              <div ref={containerRef} style={{ width: "100%", height: "60vh" }} />
               <div id='sign'>
                 <button className='button is-warning' onClick={click => setFile(null)}>Remover arquivo</button>
-                <button className='button is-info' onClick={click => assinar(f_file)}>Assinar</button>
+                <button className='button is-info' onClick={click => assinar(pdf_instance, filename)}>Assinar</button>
               </div>
             </div> :
             <div  {...getRootProps()} id="empty-content">
--- src/components/misc/misc.jsx
+++ src/components/misc/misc.jsx
@@ -1,102 +1,124 @@
-import eccrypto from "eccrypto"
 import * as pkijs from 'pkijs'
 
 import * as asn1js from 'asn1js'
-import {plainAddPlaceholder, SUBFILTER_ETSI_CADES_DETACHED, } from 'node-signpdf'
+import axios from 'axios';
 
-async function assinar(_file) {
-  console.log(_file)
-  
-  const keys = await window.crypto.subtle.generateKey(  {
+async function assinar(pdf_instance, filename) {
+
+  const keys = await window.crypto.subtle.generateKey({
     name: "ECDSA",
     namedCurve: "P-384"
   },
-  true,
-  ["sign", "verify"]
+    true,
+    ["sign", "verify"]
   )
   const pkcs10 = new pkijs.CertificationRequest();
-  
+
   pkcs10.subject.typesAndValues.push(new pkijs.AttributeTypeAndValue({
     type: "2.5.4.3",
     value: new asn1js.Utf8String({ value: "Username" })
-  }));  
-  // pkcs10.subject.typesAndValues.push(new pkijs.AttributeTypeAndValue({
-  //   type: "2.5.4.3", TODO: VER PARA CN
-  //   value: new asn1js.Utf8String({ value: "Test" })
-  // }));
-  
-  
-   await pkcs10.subjectPublicKeyInfo.importKey(keys.publicKey);
-  
-   pkcs10.attributes = [];
-  
-   // Subject Alternative Name
-   const altNames = new pkijs.GeneralNames({
-     names: [
-       new pkijs.GeneralName({ // email
-         type: 1,
-         value: "email@address.com"
-       }),
-       new pkijs.GeneralName({ // domain
-         type: 2,
-         value: "www.domain.com"
-       }),
-      ]
-    });
-    let d = _file
-    let a = plainAddPlaceholder({
-     pdfBuffer:new Buffer(_file),
-     signatureLength: 22234
-   });
-   let b = plainAddPlaceholder({
-    pdfBuffer:new Buffer(d),
-    signatureLength: 22234
-  });
-   console.log(a==b)
-   let crypto_digest = await window.crypto.subtle.digest("SHA-384", a)
-   // SubjectKeyIdentifier
-   const subjectKeyIdentifier = await window.crypto.subtle.digest({ name: "SHA-1" }, pkcs10.subjectPublicKeyInfo.subjectPublicKey.valueBlock.valueHex);
-  
-   pkcs10.attributes.push(new pkijs.Attribute({
-     type: "1.2.840.113549.1.9.14", // pkcs-9-at-extensionRequest
-     values: [(new pkijs.Extensions({
-       extensions: [
-        //  new pkijs.Extension({
-        //    extnID: "2.5.29.14", // id-ce-subjectKeyIdentifier
-        //    critical: false,
-        //    extnValue: (new asn1js.OctetString({ valueHex: subjectKeyIdentifier })).toBER(false)
-        //  }),
-         new pkijs.Extension({
-           extnID: "2.16.508.1.1.1.1", // id-ce-subjectAltName
-           critical: false,
-           extnValue: (new asn1js.Utf8String({ value: window.btoa(String.fromCharCode(...new Uint8Array(crypto_digest))) })).toBER(false)
-         }),
-        //  new pkijs.Extension({
-        //    extnID: "1.2.840.113549.1.9.7", // pkcs-9-at-challengePassword
-        //    critical: false,
-        //    extnValue: (new asn1js.PrintableString({ value: "passwordChallenge" })).toBER(false)
-        //  })
-        
-        //  new pkijs.Extension({
-        //    extnID: "2.16.508.1.1.1.1", // id-ce-subjectAltName
-        //    critical: false,
-        //    extnValue: altNames.toSchema().toBER(false)
-        //  }),
-       ]
-     })).toSchema()]
-   }));
-  
-   // Signing final PKCS#10 request
-   await pkcs10.sign(keys.privateKey, "SHA-256");
-  
-   const pkcs10Raw = pkcs10.toSchema(true).toBER();
-   const csr_contents = window.btoa(String.fromCharCode(...new Uint8Array(pkcs10Raw)));
-   const csr_file = `
-   -----BEGIN CERTIFICATE REQUEST-----
-   ${csr_contents}
-   -----END CERTIFICATE REQUEST-----
-   `
-   console.log(csr_file)
+  }));
+
+  await pkcs10.subjectPublicKeyInfo.importKey(keys.publicKey);
+
+  pkcs10.attributes = [];
+
+  await pdf_instance.signDocument({ placeholderSize: 22234 }, ({ hash, fileContents }) => {
+    return new Promise(async (resolve, reject) => {
+      let crypto_digest = await window.crypto.subtle.digest("SHA-384", fileContents)
+
+      console.log(window.btoa(String.fromCharCode(...new Uint8Array(crypto_digest))))
+      pkcs10.attributes.push(new pkijs.Attribute({
+        type: "1.2.840.113549.1.9.14", // pkcs-9-at-extensionRequest
+        values: [(new pkijs.Extensions({
+          extensions: [
+            new pkijs.Extension({
+              extnID: "2.16.508.1.1.1.1", // id-ce-subjectAltName
+              critical: false,
+              extnValue: (new asn1js.Utf8String({ value: window.btoa(String.fromCharCode(...new Uint8Array(crypto_digest))) })).toBER(false)
+            }),
+          ]
+        })).toSchema()]
+      }));
+      // Signing final PKCS#10 request
+      await pkcs10.sign(keys.privateKey, "SHA-384");
+
+      const pkcs10Raw = pkcs10.toSchema(true).toBER();
+      const csr_contents = window.btoa(String.fromCharCode(...new Uint8Array(pkcs10Raw)));
+      const csr_file = `
+-----BEGIN CERTIFICATE REQUEST-----
+${csr_contents}
+-----END CERTIFICATE REQUEST-----
+       `
+      const csr = new File([csr_file], "test.csr")
+      const form = new FormData();
+      // Pass file stream directly to form
+      form.append('csr', csr, 'file.pdf');
+      const res = await axios.post(
+        "http://kubernetes.docker.internal:5004/pki/p7b/sign",
+        form,
+        {
+          headers: {
+            //   ...form.getHeaders(),
+            // Authentication: 'Bearer token',
+            "Access-Control-Allow-Origin": "*"
+          },
+        }
+      )
+      // Load certificate in PEM encoding (base64 encoded DER)
+      const pem = res.data
+      const b64 = pem.replace(/(-----(BEGIN|END) PKCS7-----|[\n\r])/g, '')
+      // Now that we have decoded the cert it's now in DER-encoding
+      const der = Buffer(b64, 'base64')
+
+      // // And massage the cert into a BER encoded one
+      const ber = new Uint8Array(der).buffer
+      // console.log(ber)
+
+      // // And now Asn1js can decode things \o/
+      // const asn1 = asn1js.fromBER(ber);
+      // console.log(asn1.result)
+      // // if (asn1.offset === -1) {
+      // //   throw new Error("Incorrect encoded ASN.1 data");
+      // // }
+
+      // const info = new pkijs.ContentInfo({ schema: asn1.result });
+      // const data = new pkijs.SignedData({ schema: info.content });
+      // // console.log(res.data)
+      // console.log(info)
+      // console.log(data)
+      // reject()
+      resolve(stringToArrayBuffer(ber))
+      // const PKCS7Container = getPKCS7Container(hash, fileContents);
+      // if (PKCS7Container != null) {
+      //   return resolve(PKCS7Container)
+      // }
+      // reject(new Error("Could not retrieve the PKCS7 container."))
+    })
+  }).then( function () {
+    console.log("Document signed!");
+  })
+  console.log("HEEREEE")
+  let b = await window.crypto.subtle.digest("SHA-384", await pdf_instance.exportPDF())
+  console.log(window.btoa(String.fromCharCode(...new Uint8Array(b))))
+
+  const link = document.createElement('a');
+  const new_filename = filename.replace(".pdf", "assinado.pdf")
+  const blob = new Blob([await pdf_instance.exportPDF()]);
+  // Browsers that support HTML5 download attribute
+  if (link.download !== undefined) {
+    const url = URL.createObjectURL(blob);
+    link.setAttribute('href', url);
+    link.setAttribute('download', new_filename);
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  // const signer = new SignPdf()
+  // const signedPdf = signer.sign(a, Buffer.from(res.data), { passphrase: 'pass:' });
+  // const bufferPdf = Buffer.from(signedPdf)
   // var msg = crypto.createHash("sha256").update(_file).digest();
 
   // eccrypto.sign(privateKey, msg).then(function(sig) {
@@ -120,21 +142,21 @@ async function assinar(_file) {
 //   // });
 //   // const alice = createECDH('secp256k1');
 //   // const bob = createECDH('secp256k1');
-  
+
 //   // // This is a shortcut way of specifying one of Alice's previous private
 //   // // keys. It would be unwise to use such a predictable private key in a real
 //   // application.
 //   // alice.setPrivateKey(
 //   //   createHash('sha256').update('alice', 'utf8').digest()
 //   // );
-  
+
 //   // // Bob uses a newly generated cryptographically strong
 //   // // pseudorandom key pair
 //   // bob.generateKeys();
-  
+
 //   // const aliceSecret = alice.computeSecret(bob.getPublicKey(), null, 'hex');
 //   // const bobSecret = bob.computeSecret(alice.getPublicKey(), null, 'hex');
-  
+
 //   // // aliceSecret and bobSecret should be the same shared secret value
 //   // console.log(aliceSecret === bobSecret);
 //   // console.log(typeof(alice.getPrivateKey()))
@@ -144,5 +166,20 @@ async function assinar(_file) {
 //     <h1> hey </h1>
 //   )
 // }
-
+function stringToArrayBuffer(binaryString) {
+  const buffer = new ArrayBuffer(binaryString.length);
+  let bufferView = new Uint8Array(buffer);
+  for (let i = 0, len = binaryString.length; i < len; i++) {
+    bufferView[i] = binaryString.charCodeAt(i);
+  }
+  return buffer;
+}
 export default assinar;
+
+
+
+///////
+// Talvez a assinatura do pdf em si tenha que ser feita em um microserviço que use o pyhanko
+// Mesmo assim conseguimos -> criar o CSR enviar ao microserviço de PKI
+// Receber PKCS#7 e enviar a webservice que une os dois e retorna o pdf assinado
+///////
